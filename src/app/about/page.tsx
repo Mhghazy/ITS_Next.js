66ea51c578b7f0f893d3842c01dd3b5069176f1c
Mhghazy <mahmoudhghazy@hotmail.com>
2026-03-31 11:35:57 +0200

    feat: add blank about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <main className="min-h-screen bg-white">
+            <div className="container mx-auto px-4 py-16">
+                <h1>About Us</h1>
+                {/* Content goes here */}
+            </div>
+        </main>
+    );
+}
